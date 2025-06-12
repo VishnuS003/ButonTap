33c3db98b84c7504af9f2191f4bc59389e89e95c
851vishnus03 <valeriaselest3@gmail.com>
2025-06-12 13:16:45 +0300

    Тест запуска: только print

--- main.py
+++ main.py
@@ -1,21 +1,2 @@
-import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.enums import ParseMode
-from aiogram.types import Message
-
-# Инициализация бота
-bot = Bot(token="7977201566:AAHan0eTiZV4ysjmGhM4uevvLcTw4qOuqfk", parse_mode=ParseMode.HTML)
-dp = Dispatcher()
-
-@dp.message(commands=["start"])
-async def start_handler(message: Message):
-    await message.answer("Привет! Бот запущен без БД 🌷")
-
-async def main():
-    print("✅ Запуск Telegram-бота...")
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+print("✅ Python работает! 🎉")
 
